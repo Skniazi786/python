9d3437818210eda050f71eb16dfef53d0a5225c8
UkRs <theukrs@gmail.com>
2026-04-26 01:45:40 +0500

    Added a separate function for prime status. Fixed tone and removed slurs.

--- Batch 1/UkRs/Q1.py
+++ Batch 1/UkRs/Q1.py
@@ -1,15 +1,27 @@
-# Finally fixed a shitton of things.. Just need to transfer some functionality to other funcs.. or shit..
+# Fixed a few things like adding a separate function for checking prime.. still need a few minor fixes/upgrades.
+def is_prime(n):
+    i = 2
+    n_sqr = n ** 0.5
+    status = True
+    while i <= int(n_sqr):
+        if n % i == 0:
+            status = False
+            break
+        else:
+            i += 1
+    return status
+
 def main():
     while True:
-        num = input("Kindly provide the numero that you want to check the prime status for: ")
+        num = input("Kindly provide the numeber that you want to check the prime status for: ")
         if not num:
-            print("Not a fucking word huh.. Alright then.. Keep your secrets..")
+            print("You kinda need to enter something..")
             continue
         elif num.lower() == 'q' or num.lower() == 'quit':
-            print("Thank you for using this script! nigga!")
+            print("Thank you for using this script!")
             break
         elif not num.isdigit():
-            print("We need a number, you dipshit..")
+            print("Apologies for the inconvenience but we need a number.")
             continue
         else:
             n = int(num)
@@ -17,14 +29,6 @@ def main():
                 print('Kindly select a number higher than 1 to check the status.')
                 continue
             else:
-                i = 2
-                n_sqr = n ** 0.5
-                status = True
-                while i <= int(n_sqr):
-                    if n % i == 0:
-                        status = False
-                        break
-                    else:
-                        i += 1
+                status = is_prime(n)
                 print(f"{n} {'is a' if status else 'is not a'} prime number.")
 main()
