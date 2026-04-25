9bd12df21c0b44e2e6eb86467d1de5acae31ea7d
UkRs <theukrs@gmail.com>
2026-04-25 22:01:58 +0500

    Stopping here for now..

--- Batch 1/UkRs/Q1.py
+++ Batch 1/UkRs/Q1.py
@@ -1,13 +1,16 @@
 n = int(input("Kindly provide the numero that you want to check the prime status for: "))
 
 def main():
-    i = 2
-    status = 'is a'
-    while i < n:
-        if n % i == 0:
-            status = 'is not a'
-            break
-        else:
-            i += 1
-    print(f"{n} {status} prime number.")
+    if n <= 1:
+        print('Kindly select a number higher than 1 to check the status.')
+    else:
+        i = 2
+        status = True
+        while i < n:
+            if n % i == 0:
+                status = False
+                break
+            else:
+                i += 1
+        print(f"{n} {'is a' if status else 'is not a'} prime number.")
 main()
