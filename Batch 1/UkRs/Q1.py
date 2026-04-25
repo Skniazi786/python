1417e2b9050a2ddd0e5e391c409ead8266de6f0a
UkRs <theukrs@gmail.com>
2026-04-26 00:03:08 +0500

    Fixing few more stuffs.. Adding some bugs to fix later.

--- Batch 1/UkRs/Q1.py
+++ Batch 1/UkRs/Q1.py
@@ -1,20 +1,30 @@
-n = int(input("Kindly provide the numero that you want to check the prime status for: "))
-
+# Finally fixed a shitton of things.. Just need to transfer some functionality to other funcs.. or shit..
 def main():
-    if n <= 1:
-        print('Kindly select a number higher than 1 to check the status.')
-    else:
-        i = 2
-        status = True
-        while i < n:
-            if n % i == 0:
-                status = False
-                break
+    while True:
+        num = input("Kindly provide the numero that you want to check the prime status for: ")
+        if not num:
+            print("Not a fucking word huh.. Alright then.. Keep your secrets..")
+            continue
+        elif num.lower() == 'q' or num.lower() == 'quit':
+            print("Thank you for using this script! nigga!")
+            break
+        elif not num.isdigit():
+            print("We need a number, you dipshit..")
+            continue
+        else:
+            n = int(num)
+            if n <= 1:
+                print('Kindly select a number higher than 1 to check the status.')
+                continue
             else:
-                i += 1
-        print(f"{n} {'is a' if status else 'is not a'} prime number.")
+                i = 2
+                n_sqr = n ** 0.5
+                status = True
+                while i <= int(n_sqr):
+                    if n % i == 0:
+                        status = False
+                        break
+                    else:
+                        i += 1
+                print(f"{n} {'is a' if status else 'is not a'} prime number.")
 main()
-
-
-#Is this the best prime tracker function? not really..
-#But I plan to clean it up a bit more..
