05633d9cdd614b0de948386127327ab7b4a6b3c0
Skniazi786 <ssumeetkhan711@gmail.com>
2026-04-25 18:59:03 +0500

    Working on my first question but duty time >.<

--- Batch 1/Sk/Q1.py
+++ Batch 1/Sk/Q1.py
@@ -1,3 +1,18 @@
 def main():
-	pass
+	from func import cp
+	while (True):
+		num = input(cp("Type a number please I will tell you if its prime number!","b","w"))
+		if len(num)==0:
+			print(cp("Please type something, this is empty space!","r","y"))
+			continue
+		elif num.isdigit()==False:
+			print(cp("Please type a number because words don't have prime number (>.<)","r","w"))
+			continue
+		else:
+			n=int(num)
+			for i in range(1,n):
+				for e in range(1,i):
+					if i%e==0:
+						print(i)
+			break
 main()
