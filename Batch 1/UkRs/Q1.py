fa041b755b8b52684c45a9c4b300eaf7d4023a23
UkRs <theukrs@gmail.com>
2026-04-25 21:45:09 +0500

    Made some more changes..

--- Batch 1/UkRs/Q1.py
+++ Batch 1/UkRs/Q1.py
@@ -1,19 +1,13 @@
-# word = input("Kindly provide the numero that you want to check the prime status for: ")
+n = int(input("Kindly provide the numero that you want to check the prime status for: "))
 
-n = 3
 def main():
     i = 2
-    while i <= n:
-        print(n%i)
-        if n <= 0:
-            print("Kindly provide a valid number!")
-        elif n % i == 0:
-            print("It is not a prime number.")
-            break
-        elif n % i == 0 and n != i:
-            print("It is not a prime number.")
+    status = 'is a'
+    while i < n:
+        if n % i == 0:
+            status = 'is not a'
             break
         else:
-            print("It is a prime number.")
             i += 1
+    print(f"{n} {status} prime number.")
 main()
