787c7a4839b746af1b49ef1520d298401c1f0dd0
Skniazi786 <ssumeetkhan711@gmail.com>
2026-04-26 19:00:30 +0500

    working on 3rd question

---
+++ Batch 1/Sk/Q3.py
@@ -0,0 +1,13 @@
+from func import cp
+	# Importing cp function from my func.py file which only gives me good colouring for terminal.
+def Add(a,b):
+	return a+b
+def Sub(a,b):
+	return a-b
+def Multi(a,b):
+	return a*b
+def Div(a,b):
+	return a/b
+def main():
+	pass
+main()
